d6149285d2f5810dc8302e9f7ca96d2df775809c
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2023-11-23 11:42:34 -0600

    feat: added form

--- examen/Framework/Views/ContentView.swift
+++ examen/Framework/Views/ContentView.swift
@@ -9,9 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var contentViewModel = ContentViewModel()
+    @State var value = ""
+    @State var pickerValue = 0
     
     var body: some View {
-        Text("Casos")
+        ScrollView {
+            VStack {
+                Text("Casos")
+                    .font(.largeTitle)
+                Form(content: {
+                    Section(header: Text("País")) {
+                        TextField("Mexico", text: $value)
+                    }
+                    Picker(selection: $pickerValue) {
+                        Text("Casos").tag(0)
+                        Text("Muertes").tag(1)
+                    } label: {
+                        Text("Tipo de casos")
+                    }
+                    .frame(maxWidth: .infinity)
+                })
+                .frame(height: 200)
+                Button {
+                    print("log out")
+                } label: {
+                    HStack(spacing: 10) {
+                        Group {
+                            Image(systemName: "info.circle.fill")
+                                .tint(Color(UIColor.systemPurple))
+                            Text("Traer datos")
+                                .foregroundColor(Color(UIColor.systemPurple))
+                                .bold()
+                        }
+                        .padding([.top, .bottom], 10)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .overlay(
+                        Rectangle()
+                            .stroke(Color(UIColor.systemPurple), lineWidth: 1)
+                    )
+                }
+            }
+            .padding()
+        }
     }
 }
 
